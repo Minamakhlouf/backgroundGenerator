8f8a0e0721718945e13b78a78cb03204ef1901d5
Mina Makhlouf <mina.makhlouf@gmail.com>
2020-11-14 19:22:52 -0500

    adding more background choices, not refactored

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const h3 = document.querySelector("h3"); 
 const input1 = document.querySelector("#color1"); 
 const input2 = document.querySelector("#color2"); 
+const input3 = document.querySelector("#color3"); 
+const input4 = document.querySelector("#color4"); 
+const input5 = document.querySelector("#color5"); 
+const input6 = document.querySelector("#color6"); 
+const input7 = document.querySelector("#color7"); 
+const input8 = document.querySelector("#color8"); 
 const body = document.querySelector("body"); 
 const button = document.querySelector("button"); 
 const transition = document.querySelector("#transition"); 
 const hiddenDiv = document.querySelector("#hiddenDiv"); 
+const select = document.querySelector("select"); 
+const box1 = document.querySelector("#box1"); 
+const box2 = document.querySelector("#box2"); 
+const box3 = document.querySelector("#box3"); 
+const comparisonBox1 = document.querySelector("#comparisonBox1"); 
+const comparisonBox2 = document.querySelector("#comparisonBox2"); 
+const comparisonBox3 = document.querySelector("#comparisonBox3");
 
 function setGradient() {
     body.style.background = `linear-gradient(to right, ${input1.value}, ${input2.value})`; 
@@ -30,3 +43,26 @@ transition.addEventListener("click", function() {
         transition.textContent = "Click me to remove comparison box!!!"; 
     }
 }); 
+
+select.addEventListener("input", function() {
+    box1.classList.add("off"); 
+    box2.classList.add("off"); 
+    box3.classList.add("off"); 
+    switch (select.value) {
+        case "1": 
+            box1.classList.toggle("off")
+            break; 
+        case "2": 
+            box1.classList.toggle("off")
+            box2.classList.toggle("off")
+            break; 
+        case "3": 
+            box1.classList.toggle("off"); 
+            box2.classList.toggle("off"); 
+            box3.classList.toggle("off");
+            break; 
+    }
+}); 
+
+
+
